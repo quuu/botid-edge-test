0eab21bfc4bfba0aadc96c2795cff5dfca1d6c3e
Andrew Qu <qual1337@gmail.com>
2025-07-28 18:12:27 -0700

    app/layout.tsx instrumentation-client.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { BotIdClient } from "botid/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <BotIdClient protect={[{ path: "/api/post-data", method: "POST" }]} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
