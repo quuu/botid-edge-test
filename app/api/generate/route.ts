0e5b5ce25d810940fe73ff5fec8e5a664d3aedeb
Andrew Qu <qual1337@gmail.com>
2025-07-29 09:02:15 -0700

    app/api/generate/route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -2,6 +2,12 @@ import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+  const secretHeader = request.headers.get("api-secret");
+
+  console.log(secretHeader);
+  if (secretHeader !== process.env.API_SECRET) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
   const random = Math.random() * 100;
   return NextResponse.json({ message: "Data received", random });
 }
